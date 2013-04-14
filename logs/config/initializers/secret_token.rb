a969995a7d1bd57bd2e912fb3373b83d1128f42b
GitSIO2 <ya.carrasco@gmail.com>
2013-04-14 12:24:01 +0200

    first commit

---
+++ logs/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Logs::Application.config.secret_token = '36717758bbdb150330f7518e03fb8f332bdd8e5ae2e61f764da1c274f2d97e5968755e01a58ec5dbdf4d1b6416a8feedb382c1ffb39ba32b15e0ab200ae44ca0'
